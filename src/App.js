167d481eb9eef1ffc803d53529a91142f69dacf5
Kriti  Sapra <kriti.sapra18@imperial.ac.uk>
2020-08-07 11:32:39 +0100

    Using React Helmet to render meta tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from "./components/Home/Home";
 import Events from "./components/Events/Events";
@@ -8,7 +8,7 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Navbar from "./components/Navbar/Navbar";
 import GlobalStyle from "./styles/globalStyles";
 import Footer from "./components/Footer/Footer";
-import MetaTags from "react-meta-tags";
+import {Helmet} from "react-helmet";
 
 class App extends Component {
   state = {
@@ -16,31 +16,34 @@ class App extends Component {
   }
 
   handleNavbar = () => {
-    this.setState({ navbarOpen: !this.state.navbarOpen });
+    this.setState({navbarOpen: !this.state.navbarOpen});
   }
 
   render() {
     return (
       <BrowserRouter>
-        <GlobalStyle />
-        <MetaTags>
-          <title>WSET Imperial</title>
-          <meta name="description" content="WSET Imperial" />
-          <meta property="og:title" content="WSET Imperial official website" />
-          <meta property="og:image" content="../../images/wlogo.gif" />
-        </MetaTags>
+        <GlobalStyle/>
+        <Helmet>
+          <meta name="keywords"
+                content="HTML, CSS, JavaScript, Women, STEM, Science, Engineering, Technology, Students, Society, Imperial, College, London, ICL, SET, Networking"/>
+          <meta property="og:title" content="Women in SET Imperial"/>
+          <meta property="og:description"
+                content="Women in Science, Engineering and Technology Society at Imperial College London"/>
+          <meta property="og:image" content="../src/images/wlogo.gif"/>
+          <meta property="og:url" content="https://www.women-in-set-imperial.com/"/>
+        </Helmet>
         <MainDiv>
           <Navbar navbarState={this.state.navbarOpen}
-                  handleNavbar={this.handleNavbar} />
-            <PageDiv>
-              <Switch>
-                <Route path='/' component={Home} exact/>
-                <Route path='/Events' component={Events} />
-                <Route path='/Sponsors' component={Sponsors} />
-                <Route path='/About-Us' component={AboutUs} />
-                <Route component={Error}/>
-              </Switch>
-            </PageDiv>
+                  handleNavbar={this.handleNavbar}/>
+          <PageDiv>
+            <Switch>
+              <Route path='/' component={Home} exact/>
+              <Route path='/Events' component={Events}/>
+              <Route path='/Sponsors' component={Sponsors}/>
+              <Route path='/About-Us' component={AboutUs}/>
+              <Route component={Error}/>
+            </Switch>
+          </PageDiv>
           <Footer/>
         </MainDiv>
       </BrowserRouter>
@@ -58,7 +61,7 @@ const MainDiv = styled.div`
  width: 100%;
 `
 /* Grey full page background. */
-const PageDiv =  styled.div`
+const PageDiv = styled.div`
   width: 100vw;
   height: 100%;
   min-height: 100vh;
